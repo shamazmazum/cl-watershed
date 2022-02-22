dbef1d4888515de478447bf9fff97a700e0997b7
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-02-22 20:31:27 +0300

    Optimize CONVOLVE

--- src/gradient.lisp
+++ src/gradient.lisp
@@ -33,23 +33,35 @@
   "Perform generic convolution of a 2D array ARRAY with kernel
 KERNEL. Summation in the convolution formula is replaced with
 reduction using function FUNCTION."
-  (declare (type (simple-array single-float) array kernel))
+  (declare (type (simple-array single-float) array kernel)
+           (optimize (speed 3)))
   (let* ((array-dimensions  (array-dimensions array))
          (kernel-dimensions (array-dimensions kernel))
+
+         (array-height (first  array-dimensions))
+         (array-width  (second array-dimensions))
+         (kernel-height/2 (ash (first  kernel-dimensions) -1))
+         (kernel-width/2  (ash (second kernel-dimensions) -1))
+
          (result (make-array array-dimensions
                              :element-type 'single-float
                              :initial-element 0.0)))
-    (array-operations/utilities:nested-loop (i j)
-        array-dimensions
-      (setf (aref result i j)
-            (aops:reduce-index function (ik jk)
-              (let ((is (- (+ i ik) (floor (first  kernel-dimensions) 2)))
-                    (js (- (+ j jk) (floor (second kernel-dimensions) 2))))
-                (if (and (<= 0 is (1- (first  array-dimensions)))
-                         (<= 0 js (1- (second array-dimensions))))
-                    (* (aref kernel ik jk)
-                       (aref array is js))
-                    0.0)))))
+    (declare (type alex:positive-fixnum
+                   array-height array-width
+                   kernel-width/2 kernel-height/2))
+    (dotimes (i array-height)
+      (declare (type alex:non-negative-fixnum i))
+      (dotimes (j array-width)
+        (declare (type alex:non-negative-fixnum i))
+        (setf (aref result i j)
+              (aops:reduce-index function (ik jk)
+                (let ((is (- (+ i (the fixnum ik)) kernel-height/2))
+                      (js (- (+ j (the fixnum jk)) kernel-width/2)))
+                  (if (and (<= 0 is (1- array-height))
+                           (<= 0 js (1- array-width)))
+                      (* (aref kernel ik jk)
+                         (aref array is js))
+                      0.0))))))
     result))
 
 ;; Y/X
